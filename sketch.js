10ee0ab4bb4166c2e1b1262ef1b3b88e9a2e9759
preerana <79704290+preerana@users.noreply.github.com>
2021-09-30 22:42:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -53,11 +53,11 @@ function setup() {
 
 function draw() {
   background(255);
- 
-   //kangaroo.x=camera.positionX-270;
+
+  // kangaroo.x=camera.positionX-270;
   // kangaroo.x=Camera.position.x-270;
    kangaroo.x=camera.position.x-270;
-   //kangaroo.x=Camera.Position.X-270;
+  // kangaroo.x=Camera.Position.X-270;
    
   if (gameState===PLAY){
 
@@ -112,7 +112,7 @@ function spawnShrubs() {
 
   if (frameCount % 150 === 0) {
 
-    var shrub = createSprite(camera.position+500,330,40,10);
+     var shrub = createSprite(camera.position+500,330,40,10);
     // var shrub = createSprite(camera.position.x+500,330,40,10);
     // var shrub = createSprite(camera.positionX+500,330,40,10);
     // var shrub = createSprite(Camera.position.x+500,330,40,10);
@@ -144,10 +144,10 @@ function spawnShrubs() {
 function spawnObstacles() {
   if(frameCount % 120 === 0) {
 
-   // var obstacle = createSprite(camera.Position.X+400,330,40,40);
+    // var obstacle = createSprite(camera.Position.X+400,330,40,40);
     // var obstacle = createSprite(Camera.Position.x+400,330,40,40);
      var obstacle = createSprite(camera.position.x+400,330,40,40);
-     //var obstacle = createSprite(camera.position.x.400,330,40,40);
+   // var obstacle = createSprite(camera.position.x.400,330,40,40);
 
     obstacle.setCollider("rectangle",0,0,200,200)
     obstacle.addImage(obstacle1);
